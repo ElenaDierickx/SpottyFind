a3b75098403ea8376556db15bbe2c8d3206d5134
RobbeDierickx <RobbeDierickx@users.noreply.github.com>
2021-10-08 18:35:07 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,28 +6,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { LoginStackScreen } from "./Tabs/Logins/Login";
 import { Map } from "./Tabs/Map";
-
-const Tab = createBottomTabNavigator();
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyASHg7I2UOiCl8qGzB8tUdSjtg37bdWS3U",
-  authDomain: "spottyfind.firebaseapp.com",
-  databaseURL:
-    "https://spottyfind-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "spottyfind",
-  storageBucket: "spottyfind.appspot.com",
-  messagingSenderId: "587867110235",
-  appId: "1:587867110235:web:539356700169d66809f87e",
-  measurementId: "G-WY0BELDQQY",
-};
-
-const app = initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-export const database = getDatabase(app);
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -62,4 +44,21 @@ export default function App() {
   );
 }
 
+const firebaseConfig = {
+  apiKey: "AIzaSyASHg7I2UOiCl8qGzB8tUdSjtg37bdWS3U",
+  authDomain: "spottyfind.firebaseapp.com",
+  databaseURL:
+    "https://spottyfind-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "spottyfind",
+  storageBucket: "spottyfind.appspot.com",
+  messagingSenderId: "587867110235",
+  appId: "1:587867110235:web:539356700169d66809f87e",
+  measurementId: "G-WY0BELDQQY",
+};
+
+const app = initializeApp(firebaseConfig);
+
 const styles = StyleSheet.create({});
+
+// Get a reference to the database service
+//export const database = getDatabase(app);
