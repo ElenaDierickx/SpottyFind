200136c0eb32a0e1005143ee1353d9003b4e5e8e
RobbeDierickx <RobbeDierickx@users.noreply.github.com>
2021-10-13 15:40:45 +0200

    Account page

--- Tabs/Components/Button.js
+++ Tabs/Components/Button.js
@@ -1,52 +1,108 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, Pressable, Alert, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Pressable,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export function Button(props) {
-    return (
-        <Pressable style={styles.button} onPress={props.func}>
-            <Text style={styles.buttonText}>{props.children}</Text>
-        </Pressable>
-    );
+  return (
+    <Pressable style={styles.button} onPress={props.func}>
+      <Text style={styles.buttonText}>{props.children}</Text>
+    </Pressable>
+  );
+}
+
+export function SmallButton(props) {
+  return (
+    <Pressable style={styles.smallButton} onPress={props.func}>
+      <Text style={styles.smallButtonText}>{props.children}</Text>
+    </Pressable>
+  );
 }
 
 export function LocationButton(props) {
-    return (
-        <Pressable style={styles.location} onPress={props.onPress}>
-            <Ionicons style={styles.buttonIcon} name="locate-outline"></Ionicons>
-        </Pressable>
-    );
+  return (
+    <Pressable style={styles.location} onPress={props.onPress}>
+      <Ionicons style={styles.buttonIcon} name="locate-outline"></Ionicons>
+    </Pressable>
+  );
+}
+
+export function StatButton(props) {
+  return (
+    <Pressable style={styles.StatButton} onPress={props.func}>
+      <Text style={styles.StatButtonText}>{props.children}</Text>
+      <Text style={styles.StatButtonText}>></Text>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: "#2CCB33",
-        borderRadius: 5,
-        height: 50,
-        marginLeft: 20,
-        marginRight: 20,
-        paddingLeft: 5,
-        marginBottom: 10,
-        justifyContent: "center",
-    },
-    buttonText: {
-        fontWeight: "bold",
-        fontSize: 24,
-        alignSelf: "center",
-        color: "#FFFFFF",
-    },
-    location: {
-        backgroundColor: "#2CCB33",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 10,
-        borderRadius: 100,
-        top: 40,
-        right: 20,
-        position: "absolute",
-    },
-    buttonIcon: {
-        fontSize: 30,
-        color: "white",
-    },
+  button: {
+    backgroundColor: "#2CCB33",
+    borderRadius: 5,
+    height: 50,
+    marginLeft: 20,
+    marginRight: 20,
+    paddingLeft: 5,
+    marginBottom: 10,
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 24,
+    alignSelf: "center",
+    color: "#FFFFFF",
+  },
+  location: {
+    backgroundColor: "#2CCB33",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 100,
+    top: 40,
+    right: 20,
+    position: "absolute",
+  },
+  buttonIcon: {
+    fontSize: 30,
+    color: "white",
+  },
+  smallButton: {
+    backgroundColor: "#2CCB33",
+    borderRadius: 5,
+    height: 40,
+    width: 100,
+    paddingLeft: 5,
+    marginBottom: 10,
+    justifyContent: "center",
+    marginTop: 50,
+    alignSelf: "flex-end",
+    marginRight: 25,
+  },
+  smallButtonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    alignSelf: "center",
+    color: "#FFFFFF",
+  },
+  StatButton: {
+    height: 50,
+    marginLeft: 20,
+    marginRight: 20,
+    paddingLeft: 5,
+    marginBottom: 10,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  StatButtonText: {
+    fontWeight: "bold",
+    fontSize: 24,
+    alignSelf: "center",
+  },
 });
