1a1144d38befe9863fd4216d08d23bfdc766497b
RobbeDierickx <RobbeDierickx@users.noreply.github.com>
2021-10-18 13:43:52 +0200

    Following and Follower stats

--- Tabs/People/User.js
+++ Tabs/People/User.js
@@ -1,15 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Image,
-  Pressable,
-  Alert,
-  VirtualizedList,
-} from "react-native";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import {
   Button,
@@ -18,14 +9,17 @@ import {
   StatButton,
 } from "./../Components/Button";
 import Firebase from "../../Config/Firebase";
+import { useFocusEffect } from "@react-navigation/native";
 
 export function UserScreen({ route, navigation }) {
   const { uid } = route.params;
   const [username, setUsername] = useState("");
   const [account, setAccount] = useState("");
   const [following, setFollowing] = useState(false);
+  const [followingStat, setFollowingStat] = useState(0);
+  const [followersStat, setFollowersStat] = useState(0);
 
-  useEffect(() => {
+  const onRender = function () {
     Firebase.firestore()
       .collection("following")
       .where("follower", "==", Firebase.auth().currentUser.uid)
@@ -35,6 +29,20 @@ export function UserScreen({ route, navigation }) {
         if (doc.size > 0) {
           setFollowing(true);
         }
+      });
+    Firebase.firestore()
+      .collection("following")
+      .where("following", "==", uid)
+      .get()
+      .then((doc) => {
+        setFollowersStat(doc.size);
+      });
+    Firebase.firestore()
+      .collection("following")
+      .where("follower", "==", uid)
+      .get()
+      .then((doc) => {
+        setFollowingStat(doc.size);
       });
     setAccount(Firebase.auth().currentUser.uid);
     if (uid) {
@@ -48,7 +56,9 @@ export function UserScreen({ route, navigation }) {
           }
         });
     }
-  }, []);
+  };
+
+  useFocusEffect(onRender);
 
   const followUser = async () => {
     if (following) {
@@ -94,11 +104,11 @@ export function UserScreen({ route, navigation }) {
           <Text style={styles.statText}>Spots</Text>
         </View>
         <View>
-          <Text style={styles.statNumber}>23</Text>
+          <Text style={styles.statNumber}>{followersStat}</Text>
           <Text style={styles.statText}>Followers</Text>
         </View>
         <View>
-          <Text style={styles.statNumber}>23</Text>
+          <Text style={styles.statNumber}>{followingStat}</Text>
           <Text style={styles.statText}>Following</Text>
         </View>
         <View>
