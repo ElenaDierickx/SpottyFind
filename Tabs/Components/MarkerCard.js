28b2cfefd4a48e0e172008b48f7d4e35ec4489bd
RobbeDierickx <RobbeDierickx@users.noreply.github.com>
2021-11-08 13:38:20 +0100

    Location Card

--- Tabs/Components/MarkerCard.js
+++ Tabs/Components/MarkerCard.js
@@ -1,38 +1,92 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput, Pressable, Image } from "react-native";
-import { CardButton } from "./Button";
-import { Ionicons } from "@expo/vector-icons";
-import { addLocationImage } from "../../utils/Imaging";
-import { addMarker } from "../../utils/MapHelper";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { getMarkerImage } from "../../utils/MapHelper";
+import { Ionicons } from "@expo/vector-icons";
+import { CardButton } from "./Button";
 
 export function MarkerCard(props) {
-    return (
-        <View style={styles.markerCard}>
-            <Text style={styles.title}>{props.marker.title}</Text>
-            <Image style={styles.image} source={image} />
-            <Text style={styles.descriptionTitle}>Description</Text>
-            <Text style={styles.description}>{description}</Text>
+  const [image, setImage] = useState(null);
+
+  const imageToLoad = image
+    ? { uri: image }
+    : require("./../../img/account.png");
+
+  const onRender = async () => {
+    const imageTemp = await getMarkerImage(props.marker.id);
+    setImage(imageTemp);
+  };
+
+  useEffect(() => {
+    onRender();
+  }, []);
+
+  return (
+    <View style={styles.markerCard}>
+      <Text style={styles.title}>{props.marker.data().title}</Text>
+      <Image style={styles.image} source={imageToLoad} />
+      <View style={styles.reviewView}>
+        <Text style={styles.score}>4,3</Text>
+        <View style={styles.stars}>
+          <Ionicons style={styles.star} name="star"></Ionicons>
+          <Ionicons style={styles.star} name="star"></Ionicons>
+          <Ionicons style={styles.star} name="star"></Ionicons>
+          <Ionicons style={styles.star} name="star"></Ionicons>
+          <Ionicons style={styles.star} name="star"></Ionicons>
         </View>
-    );
+        <CardButton style={styles.reviewButton}>Review</CardButton>
+      </View>
+      <Text style={styles.descriptionTitle}>Description</Text>
+      <Text style={styles.description}>{props.marker.data().description}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    markerCard: {
-        height: 460,
-        width: "95%",
-        backgroundColor: "#FFFFFF",
-        position: "absolute",
-        bottom: 10,
-        alignSelf: "center",
-        borderRadius: 20,
-        padding: 20,
-        alignContent: "center",
-    },
-    title: {
-        fontSize: 20,
-    },
-    image: {},
-    descriptionTitle: {},
-    description: {},
+  markerCard: {
+    height: 460,
+    width: "95%",
+    backgroundColor: "#FFFFFF",
+    position: "absolute",
+    bottom: 10,
+    alignSelf: "center",
+    borderRadius: 20,
+    padding: 20,
+    alignContent: "center",
+  },
+  title: {
+    fontSize: 40,
+  },
+  image: {
+    marginTop: 10,
+    width: 300,
+    height: 200,
+    borderRadius: 10,
+  },
+  descriptionTitle: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  description: {},
+  reviewView: {
+    flexDirection: "row",
+
+    marginTop: 10,
+  },
+  score: {
+    fontSize: 40,
+  },
+  stars: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginLeft: 20,
+  },
+  star: {
+    color: "yellow",
+    fontSize: 20,
+  },
+  reviewButton: {
+    alignSelf: "center",
+    marginLeft: 30,
+  },
 });
--- Tabs/Map.js
+++ Tabs/Map.js
@@ -9,114 +9,126 @@ import { getMarkers } from "../utils/MapHelper";
 import { MarkerCard } from "./Components/MarkerCard";
 
 export function Map() {
-    const [location, setLocation] = useState(null);
-    const [addLocationOn, setaddLocationOn] = useState(false);
-    const [markers, setMarkers] = useState([]);
-    const [markerCard, setMarkerCard] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [addLocationOn, setaddLocationOn] = useState(false);
+  const [markers, setMarkers] = useState([]);
+  const [markerCard, setMarkerCard] = useState(null);
 
-    const gettingMarkers = async () => {
-        var markers = await getMarkers();
-        var index = 0;
-        var markerlist = [];
+  const gettingMarkers = async () => {
+    var markers = await getMarkers();
+    var index = 0;
+    var markerlist = [];
 
-        markers.forEach((marker) => {
-            markerlist.push(
-                <Marker
-                    key={index}
-                    coordinate={{ latitude: marker.data().location.coords.latitude, longitude: marker.data().location.coords.longitude }}
-                    title={marker.data().title}
-                    description={marker.data().description}
-                    onPress={() => {
-                        setMarkerCard(marker.data());
-                    }}
-                />
-            );
-            index++;
-        });
+    markers.forEach((marker) => {
+      markerlist.push(
+        <Marker
+          key={index}
+          coordinate={{
+            latitude: marker.data().location.coords.latitude,
+            longitude: marker.data().location.coords.longitude,
+          }}
+          title={marker.data().title}
+          description={marker.data().description}
+          onPress={() => {
+            setMarkerCard(marker);
+          }}
+        />
+      );
+      index++;
+    });
 
-        setMarkers(markerlist);
-    };
+    setMarkers(markerlist);
+  };
 
-    useEffect(() => {
-        getLocation();
+  useEffect(() => {
+    getLocation();
 
-        gettingMarkers();
-    }, []);
+    gettingMarkers();
+  }, []);
 
-    async function getLocation() {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-            setErrorMsg("Permission to access location was denied");
-            return;
-        }
-        let location = await Location.getLastKnownPositionAsync({});
-        if (location) {
-            location.latitudeDelta = 0.2;
-            location.longitudeDelta = 0.2;
-            setLocation(location);
-        }
+  async function getLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
     }
+    let location = await Location.getLastKnownPositionAsync({});
+    if (location) {
+      location.latitudeDelta = 0.2;
+      location.longitudeDelta = 0.2;
+      setLocation(location);
+    }
+  }
 
-    const locationH = location ? location : { coords: { latitude: 50.8503, longitude: 4.3517 }, latitudeDelta: 2, longitudeDelta: 2 };
+  const locationH = location
+    ? location
+    : {
+        coords: { latitude: 50.8503, longitude: 4.3517 },
+        latitudeDelta: 2,
+        longitudeDelta: 2,
+      };
 
-    return (
-        <View style={styles.container}>
-            <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
 
-            <MapView
-                showsMyLocationButton={false}
-                provider={PROVIDER_GOOGLE}
-                style={styles.map}
-                followUserLocation={true}
-                showsUserLocation={true}
-                region={{
-                    latitude: locationH.coords.latitude,
-                    longitude: locationH.coords.longitude,
-                    latitudeDelta: locationH.latitudeDelta,
-                    longitudeDelta: locationH.longitudeDelta,
-                }}
-                onPress={() => {
-                    if (addLocationOn) {
-                        setaddLocationOn(false);
-                    }
-                }}
-            >
-                {markers}
-            </MapView>
+      <MapView
+        showsMyLocationButton={false}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        followUserLocation={true}
+        showsUserLocation={true}
+        region={{
+          latitude: locationH.coords.latitude,
+          longitude: locationH.coords.longitude,
+          latitudeDelta: locationH.latitudeDelta,
+          longitudeDelta: locationH.longitudeDelta,
+        }}
+        onPress={() => {
+          if (addLocationOn) {
+            setaddLocationOn(false);
+          }
+          if (markerCard) {
+            setMarkerCard(null);
+          }
+        }}
+      >
+        {markers}
+      </MapView>
 
-            <LocationButton
-                onPress={() => {
-                    getLocation();
-                }}
-            />
+      <LocationButton
+        onPress={() => {
+          getLocation();
+        }}
+      />
 
-            <AddLocationButton
-                onPress={() => {
-                    setaddLocationOn(true);
-                }}
-            />
+      <AddLocationButton
+        onPress={() => {
+          setaddLocationOn(true);
+        }}
+      />
 
-            {addLocationOn && (
-                <AddLocationCard
-                    backFunc={() => {
-                        setaddLocationOn(false);
-                    }}
-                />
-            )}
+      {addLocationOn && (
+        <AddLocationCard
+          backFunc={() => {
+            setaddLocationOn(false);
+          }}
+        />
+      )}
 
-            {markerCard && <MarkerCard marker={markerCard} />}
-        </View>
-    );
+      {markerCard && <MarkerCard marker={markerCard} />}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        justifyContent: "center",
-    },
-    map: {
-        flex: 1,
-        zIndex: -1,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
+    zIndex: -1,
+  },
 });
